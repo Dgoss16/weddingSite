fc22ffa573248d08a3586c44cd3d5419afe7869d
Dylan Goss <dylangoss21@gmail.com>
2020-03-28 14:44:16 -0500

    fixed simple text

--- src/Components/Registry/Registry.js
+++ src/Components/Registry/Registry.js
@@ -15,7 +15,7 @@ export default class Home extends Component {
             <div className='registryComponent'>
                 <Navbar/>
                 <div className='registryComponentTitleConatiner'>
-                    <h1 className='title'>Click and image below for places we've register with:</h1>
+                    <h1 className='title'>Click an image below for places we've register with:</h1>
                 </div>
 
                 <div className='registryComponentIconsContainer'>
